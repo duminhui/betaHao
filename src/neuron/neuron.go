63ceee8a74d9bde7e13360394a863faacf813fe1
duminhui <duminghui@126.com>
2016-06-27 00:37:07 +0800

    a design of neuron excit transmission

--- src/neuron/neuron.go
+++ src/neuron/neuron.go
@@ -40,14 +40,40 @@ type Neuron struct {
 	trans Transmission
 }
 
+//TODO: however pseudocode now
 func (nn *Neuron) pass_potential() {
 	this := nn
 	next := caculate_next_neuron_present_status()
 	if(next.in_resting_period()) {
+		// if next neuron need recovered, then this neuron should not trans its excited and its excited_p should decrease
 		this.trans.Decrease()
-	} else if(next.in_activing_period()) {
-		merge_probability()
-		next.try_excit()
-
+	} else {
+		if(next.in_activing_period()) {
+			temp_p = merge_probability()
+			if(next.try_enough_energy()) {
+				if (next.try_excite()) { // if could be excited,
+					next.cell.pool.Decrease() // decrease next energy
+					this.trans.Increase()
+					push_next_neuron_into_dequeue() 
+					change_next_neuron_state(resing_state) // let next be into resting_state
+				} else {
+					next.cell.excit_p = temp_p
+					// TODO: should there be a decrease of this.trans
+				}
+			} else { // not enough energy,
+				next.try_avergy_pre_neurons()
+			}
+		} else { // in scilent state
+			temp_p = next.cell.base_p
+			if(next.try_enough_energy() { 
+				next.cell.pool.Decrease()
+				this.trans.Increase()
+				push_next_neuron_into_dequeue()
+				change_next_neuron_state(resting_state) // just to mark a timestamp
+			} else {
+				change_next_neuron_state(activing_state) // just to mark a timestamp
+				// TODO: should there be a decrease of this.trans, maybe a distinguishing of growing up and mature is needed
+			}
+		}
 	}
 }
